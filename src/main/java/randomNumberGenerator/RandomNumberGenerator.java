2226b7c3478cfeed1b788729d24e2ace60337c22
Starovoytov <migellitta@gmail.com>
2020-12-01 18:44:38 +0300

    optimized rng

--- src/main/java/randomNumberGenerator/RandomNumberGenerator.java
+++ src/main/java/randomNumberGenerator/RandomNumberGenerator.java
@@ -1,30 +1,10 @@
 package randomNumberGenerator;
-
 import java.util.Arrays;
 
 public class RandomNumberGenerator {
     public void getNumbers(String[] args) {
         if (args.length != 0) {
-            String incomingArgs = Arrays.toString(args);
-            int argsLengthForOnlyNumbers = args.length;
-            for (String element : args) {
-                try {
-                    double temp = Double.parseDouble(element.replace(",", "."));
-                } catch (Exception e) {
-                    argsLengthForOnlyNumbers--;
-                }
-            }
-            int[] argsOnlyNumbers = new int[argsLengthForOnlyNumbers];
-            int amendment = 0;
-            for (int i = 0; i < args.length; i++) {
-                try {
-                    double temp = Double.parseDouble(args[i].replace(",", "."));
-                    int roundedTemp = (int) Math.floor(temp);
-                    argsOnlyNumbers[i - amendment] = roundedTemp;
-                } catch (Exception e) {
-                    amendment++;
-                }
-            }
+            int[] argsOnlyNumbers = getOnlyNumbers(args);
             for (int i = 0; i < argsOnlyNumbers.length; i++) {
                 if (argsOnlyNumbers[i] > 0) {
                     long maxValue = (long) (Math.pow(10, argsOnlyNumbers[i]));
@@ -37,5 +17,30 @@ public class RandomNumberGenerator {
         } else {
             System.out.println("No incoming values");
         }
+    }
+
+    @org.jetbrains.annotations.NotNull
+    private int[] getOnlyNumbers(String[] args) {
+        String incomingArgs = Arrays.toString(args);
+        int argsLengthForOnlyNumbers = args.length;
+        for (String element : args) {
+            try {
+                double temp = Double.parseDouble(element.replace(",", "."));
+            } catch (Exception e) {
+                argsLengthForOnlyNumbers--;
+            }
+        }
+        int[] argsOnlyNumbers = new int[argsLengthForOnlyNumbers];
+        int amendment = 0;
+        for (int i = 0; i < args.length; i++) {
+            try {
+                double temp = Double.parseDouble(args[i].replace(",", "."));
+                int roundedTemp = (int) Math.floor(temp);
+                argsOnlyNumbers[i - amendment] = roundedTemp;
+            } catch (Exception e) {
+                amendment++;
+            }
+        }
+        return argsOnlyNumbers;
     }
 }
