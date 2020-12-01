1a46592642633d204ed29116a2f8ac5d2d5af1a4
Starovoytov <migellitta@gmail.com>
2020-12-01 18:28:11 +0300

    finished rng

--- src/main/java/randomNumberGenerator/RandomNumberGenerator.java
+++ src/main/java/randomNumberGenerator/RandomNumberGenerator.java
@@ -18,17 +18,21 @@ public class RandomNumberGenerator {
             int amendment = 0;
             for (int i = 0; i < args.length; i++) {
                 try {
-                    int temp = Integer.parseInt(args[i].replace(",", "."));
-                    int roundedTemp = (int)Math.floor(temp);
+                    double temp = Double.parseDouble(args[i].replace(",", "."));
+                    int roundedTemp = (int) Math.floor(temp);
                     argsOnlyNumbers[i - amendment] = roundedTemp;
                 } catch (Exception e) {
                     amendment++;
                 }
             }
             for (int i = 0; i < argsOnlyNumbers.length; i++) {
-                long maxValue = (long) (Math.pow(10, argsOnlyNumbers[i]));
-                long randomNumber = (long)(Math.random() * maxValue * 2 - maxValue);
-                System.out.println(argsOnlyNumbers[i] + " : " + randomNumber);
+                if (argsOnlyNumbers[i] > 0) {
+                    long maxValue = (long) (Math.pow(10, argsOnlyNumbers[i]));
+                    long randomNumber = (long) (Math.random() * maxValue * 2 - maxValue);
+                    System.out.println(argsOnlyNumbers[i] + " : " + randomNumber);
+                } else {
+                    System.out.println(argsOnlyNumbers[i] + " : Incorrect amount of numerals");
+                }
             }
         } else {
             System.out.println("No incoming values");
